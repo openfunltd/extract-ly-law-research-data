b60556d71185e727c356f379a4141f6fef3c8be9
typingmonk <tmonk.hsieh@tmonk.tw>
2024-07-12 20:00:44 +0800

    Initialize project

---
+++ main.php
@@ -0,0 +1,4 @@
+<?php
+include 'src/Initialer.inc.php';
+
+Initialer::initalizeProject(); //建立資料夾 doc/ html/ csv/
---
+++ src/Initialer.inc.php
@@ -0,0 +1,15 @@
+<?php
+
+class Initialer {
+    public static function initalizeProject() {
+        if (! file_exists('doc')) {
+            mkdir('doc');
+        }
+        if (! file_exists('html')) {
+            mkdir('html');
+        }
+        if (! file_exists('csv')) {
+            mkdir('csv');
+        }
+    }
+}
