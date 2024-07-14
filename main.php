26eb6356f088eba09a7b76bf3c2aa50af33a2f09
typingmonk <tmonk.hsieh@tmonk.tw>
2024-07-15 01:52:15 +0800

    Request 議題研析 and packing csv rows

--- main.php
+++ main.php
@@ -1,11 +1,26 @@
 <?php
+include 'src/Downloader.inc.php';
 include 'src/Initialer.inc.php';
 include 'src/RelatedLaws.inc.php';
 
 Initialer::initalizeProject(); //建立資料夾 doc/ html/ csv/
 
-$files = array_slice(scandir('html/'), 2);
+$reports = Downloader::queryAPI();
+$rows = [];
+$rows[] = ['research_no', 'title', 'published_date', 'authors', 'file_path'];
+foreach ($reports as $research) {
+    $row = [];
+    $row[] = trim($research->{'@LawReportNo'});
+    $row[] = trim($research->Title);
+    $row[] = substr($research->{'@CompletionDate'}, 0, 10);
+    $row[] = trim($research->{'@Author'});
+    $row[] = trim($research->FilePath);
+    $rows[] = $row;
+}
+var_dump($rows);
 
+/*
+$files = array_slice(scandir('html/'), 2);
 $rows = [];
 $rows[] = ['research_no', 'answer', 'pattern001', 'pattern002', 'pattern003', 'pattern004'];
 foreach ($files as $filename) {
@@ -22,3 +37,4 @@ foreach ($rows as $row) {
     fputcsv($fp, $row);
 }
 fclose($fp);
+*/
---
+++ src/Downloader.inc.php
@@ -0,0 +1,23 @@
+<?php
+
+class Downloader {
+    public static function queryAPI() {
+        $year = date('Y', strtotime('-1 days'));
+        $yearROC = $year - 1911;
+        $date = date('md',strtotime('-1 days'));
+        $date = $yearROC . $date;
+
+        //TODO to be removed
+        $date = '1130711';
+
+        $query = sprintf('https://www.ly.gov.tw/WebAPI/LawBureauResearch.aspx?type=議題研析&from=%s&to=%s&mode=json', $date, $date);
+        $res = file_get_contents($query);
+
+        if (trim($res) == '{}') {
+            return [];
+        }
+
+        $json = json_decode($res);
+        return $json->LawBureauResearch->Category->Report;
+    }
+}
